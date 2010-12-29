d9cc9ab4ffd58bd36249a199d75ddd1639b976d4
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2010-12-29 13:50:40 +0000

    Added auto-inclusion of Validator

--- SubPageList.php
+++ SubPageList.php
@@ -25,6 +25,16 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
+// Include the Validator extension if that hasn't been done yet, since it's required for SubPageList to work.
+if ( !defined( 'Validator_VERSION' ) ) {
+	@include_once( dirname( __FILE__ ) . '/../Validator/Validator.php' );
+}
+
+// Only initialize the extension when all dependencies are present.
+if ( ! defined( 'Validator_VERSION' ) ) {
+	die( '<b>Error:</b> You need to have <a href="http://www.mediawiki.org/wiki/Extension:Validator">Validator</a> installed in order to use <a href="http://www.mediawiki.org/wiki/Extension:SubPageList">SubPageList</a>.<br />' );
+}
+
 define( 'SPL_VERSION', '0.1 alpha' );
 
 $wgExtensionCredits['other'][] = array(

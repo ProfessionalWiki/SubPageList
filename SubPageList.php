3bb84ea7714a3dd6b7b12c5a21fdf91c0afb3454
jeroendedauw <jeroendedauw@gmail.com>
2013-01-29 16:10:40 +0100

    align autoloads
    Change-Id: I9c3010ad80f6440fd6250731578fd5373e49997d

--- SubPageList.php
+++ SubPageList.php
@@ -79,14 +79,14 @@ call_user_func( function() {
 	$wgAutoloadClasses['SubPageList'] = __DIR__ . '/todo/SubPageList.php';
 	$wgAutoloadClasses['SubPageCount'] = __DIR__ . '/todo/SubPageCount.php';
 
-	$wgAutoloadClasses['SubPageList\CacheInvalidator'] = __DIR__ . '/includes/CacheInvalidator.php';
-	$wgAutoloadClasses['SubPageList\DBConnectionProvider'] = __DIR__ . '/includes/DBConnectionProvider.php';
-	$wgAutoloadClasses['SubPageList\Extension'] = __DIR__ . '/includes/Extension.php';
-	$wgAutoloadClasses['SubPageList\LazyDBConnectionProvider'] = __DIR__ . '/includes/LazyDBConnectionProvider.php';
-	$wgAutoloadClasses['SubPageList\Settings'] = __DIR__ . '/includes/Settings.php';
-	$wgAutoloadClasses['SubPageList\SimpleCacheInvalidator'] = __DIR__ . '/includes/SimpleCacheInvalidator.php';
-	$wgAutoloadClasses['SubPageList\SimpleSubPageFinder'] = __DIR__ . '/includes/SimpleSubPageFinder.php';
-	$wgAutoloadClasses['SubPageList\SubPageFinder'] = __DIR__ . '/includes/SubPageFinder.php';
+	$wgAutoloadClasses['SubPageList\CacheInvalidator'] 			= __DIR__ . '/includes/CacheInvalidator.php';
+	$wgAutoloadClasses['SubPageList\DBConnectionProvider'] 		= __DIR__ . '/includes/DBConnectionProvider.php';
+	$wgAutoloadClasses['SubPageList\Extension'] 				= __DIR__ . '/includes/Extension.php';
+	$wgAutoloadClasses['SubPageList\LazyDBConnectionProvider'] 	= __DIR__ . '/includes/LazyDBConnectionProvider.php';
+	$wgAutoloadClasses['SubPageList\Settings'] 					= __DIR__ . '/includes/Settings.php';
+	$wgAutoloadClasses['SubPageList\SimpleCacheInvalidator'] 	= __DIR__ . '/includes/SimpleCacheInvalidator.php';
+	$wgAutoloadClasses['SubPageList\SimpleSubPageFinder'] 		= __DIR__ . '/includes/SimpleSubPageFinder.php';
+	$wgAutoloadClasses['SubPageList\SubPageFinder'] 			= __DIR__ . '/includes/SubPageFinder.php';
 
 
 	$wgExtensionFunctions[] = function() {

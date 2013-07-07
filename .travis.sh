122ec231a332b6ee0ba99a74516eaf9b95f46109
jeroendedauw <jeroendedauw@gmail.com>
2013-07-07 20:33:32 +0200

    Added TravisCI config
    Change-Id: I8cfd6815d447a4b2d4a64134dad070f2ab6e3d2c

---
+++ .travis.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+set -x
+
+cd ..
+
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/core.git phase3 --depth 1
+
+cd phase3
+
+mysql -e 'create database its_a_mw;'
+php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin
+
+cd extensions
+composer create-project wikibase/query:dev-master WikibaseQuery --keep-vcs
+
+cd ..
+echo 'require_once( __DIR__ . "/extensions/WikibaseQuery/WikibaseQuery.php" );' >> LocalSettings.php
+
+echo 'error_reporting(E_ALL| E_STRICT);' >> LocalSettings.php
+echo 'ini_set("display_errors", 1);' >> LocalSettings.php
+echo '$wgShowExceptionDetails = true;' >> LocalSettings.php
+echo '$wgDevelopmentWarnings = true;' >> LocalSettings.php
+
+php maintenance/update.php --quick

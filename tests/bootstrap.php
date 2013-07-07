544287b6640331a5d8a06503b0f47c67890176a8
jeroendedauw <jeroendedauw@gmail.com>
2013-07-07 20:27:01 +0200

    Added phpunit.xml.dist config file and bootstrap
    Change-Id: I33785f2569a928620c197f96330327c9aa915066

---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * PHPUnit bootstrap file for the SubPageList extension.
+ *
+ * @since 0.1
+ *
+ * @file
+ * @ingroup SPL
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+
+require_once( __DIR__ . '/evilMediaWikiBootstrap.php' );
+
+require_once( __DIR__ . '/../SubPageList.php' );

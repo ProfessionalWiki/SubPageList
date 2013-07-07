46eecf2fc2bc79b60b6941027799e42bf95175fb
jeroendedauw <jeroendedauw@gmail.com>
2013-07-07 23:15:34 +0200

    Fix TravisCI build config
    Change-Id: Ibb451c7c3fd32669a10db90eee0425b2a0be662a

--- .travis.sh
+++ .travis.sh
@@ -19,10 +19,10 @@ cd ../phase3
 mysql -e 'create database its_a_mw;'
 php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin
 
-cd extensions
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/SubPageList.git --depth 1
+cd extensions/SubPageList
+composer install
 
-cd ..
+cd ../..
 echo 'require_once( __DIR__ . "/extensions/SubPageList/SubPageList.php" );' >> LocalSettings.php
 
 echo 'error_reporting(E_ALL| E_STRICT);' >> LocalSettings.php

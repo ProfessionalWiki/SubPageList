2333cea5d7c85a0032c59bd282cd77dbefcf4297
jeroendedauw <jeroendedauw@gmail.com>
2013-11-29 23:53:33 +0100

    Autoloading and dependency imporvements
    Change-Id: I629782b53bfe308ec4ae636f02e4a0d29739ec4c

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -11,28 +11,8 @@
 
 require_once( __DIR__ . '/evilMediaWikiBootstrap.php' );
 
-$wgNamespacesWithSubpages[NS_MAIN] = true;
+$GLOBALS['wgNamespacesWithSubpages'][NS_MAIN] = true;
 
-require_once( __DIR__ . '/../SubPageList.php' );
+echo exec( 'composer update' ) . "\n";
 
-spl_autoload_register( function ( $className ) {
-	$className = ltrim( $className, '\\' );
-	$fileName = '';
-	$namespace = '';
-
-	if ( $lastNsPos = strripos( $className, '\\') ) {
-		$namespace = substr( $className, 0, $lastNsPos );
-		$className = substr( $className, $lastNsPos + 1 );
-		$fileName  = str_replace( '\\', '/', $namespace ) . '/';
-	}
-
-	$fileName .= str_replace( '_', '/', $className ) . '.php';
-
-	$namespaceSegments = explode( '\\', $namespace );
-
-	if ( $namespaceSegments[0] === 'Tests' ) {
-		if ( count( $namespaceSegments ) > 2 && $namespaceSegments[2] === 'SubPageList' ) {
-			require_once __DIR__ . '/../' . $fileName;
-		}
-	}
-} );
+require_once( __DIR__ . '/../vendor/autoload.php' );

ce70d16cbcacc0b73d84a9dc6a566af154537e31
jeroendedauw <jeroendedauw@gmail.com>
2014-06-26 00:32:13 +0200

    Fix coverage clover file path

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -6,7 +6,7 @@ cd ../phase3/extensions/SubPageList
 
 if [ "$MW-$DBTYPE" == "master-mysql" ]
 then
-	phpunit --coverage-clover ../../extensions/SubPageList/build/logs/clover.xml
+	phpunit --coverage-clover ../../extensions/SubPageList/build/coverage.clover
 else
 	phpunit
 fi

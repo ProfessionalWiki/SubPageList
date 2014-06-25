ce0bf162d27965bc75768657d02e13d42b94aec5
jeroendedauw <jeroendedauw@gmail.com>
2014-06-25 23:15:21 +0200

    Use PSR-4

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -9,10 +9,19 @@
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 
+if ( php_sapi_name() !== 'cli' ) {
+	die( 'Not an entry point' );
+}
+
+error_reporting(E_ALL| E_STRICT);
+ini_set("display_errors", 1);
+
 require_once( __DIR__ . '/evilMediaWikiBootstrap.php' );
 
 $GLOBALS['wgNamespacesWithSubpages'][NS_MAIN] = true;
 
-echo exec( 'composer update' ) . "\n";
+if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
+	die( 'You need to install this package with Composer before you can run the tests' );
+}
 
-require_once( __DIR__ . '/../vendor/autoload.php' );
+$autoLoader = require_once( __DIR__ . '/../vendor/autoload.php' );

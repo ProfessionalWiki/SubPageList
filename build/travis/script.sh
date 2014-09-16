0ee7d090f217dc88ce29db4ddfb003bf4e5094ed
jeroendedauw <jeroendedauw@gmail.com>
2014-09-16 20:55:19 +0200

    Simplify PHPUnit run on Travis

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -8,7 +8,7 @@ cd ../phase3/tests/phpunit
 
 if [ "$TYPE" == "coverage" ]
 then
-	php phpunit.php --group SubPageList -c ../../extensions/SubPageList/phpunit.xml.dist --coverage-clover $originalDirectory/build/coverage.clover
+	php phpunit.php -c ../../extensions/SubPageList/phpunit.xml.dist --coverage-clover $originalDirectory/build/coverage.clover
 else
-	php phpunit.php --group SubPageList -c ../../extensions/SubPageList/phpunit.xml.dist
+	php phpunit.php -c ../../extensions/SubPageList/phpunit.xml.dist
 fi

d484e57d690e09aa2e07d30b3b8b62b2197cc01d
Jeroen De Dauw <jeroendedauw@gmail.com>
2016-06-18 12:17:00 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,8 +13,8 @@ if ( PHP_SAPI !== 'cli' ) {
 	die( 'Not an entry point' );
 }
 
-error_reporting(E_ALL| E_STRICT);
-ini_set("display_errors", 1);
+error_reporting( 1 );
+ini_set( 'display_errors', 1 );
 
 $GLOBALS['wgNamespacesWithSubpages'][NS_MAIN] = true;
 
